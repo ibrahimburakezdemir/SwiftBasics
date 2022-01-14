6f0aa44738fccdc104db8f5b4977ec3ad82454f8
ibrahim Burak Ezdemir <ibrahimezdemir@gmail.com>
2022-01-14 14:04:29 +0300

    Initial Commit

---
+++ Swift Basics.playground/Pages/Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+//-------------PART  1 ---------------
+
+//String
+
+var greeting = "Hello, playground"
+
+var userName = "James"
+userName.append("o")
+userName.lowercased()
+userName.uppercased()
+
+var userSurname = "Black"
+
+//Integer & Double & Float
+
+var myNumber = 5*4
+myNumber = 7*8
+
+let pi = 3.14
+let radius = 50.0
+
+radius * pi
+
+//Double
+let userAge = 30
+let number1 = 4
+
+userAge/number1
+
+let userAgeD = 30.0
+let number2 = 4.0
+
+userAgeD/number2
+
+//Boolean
+
+var myBoolean = true
+myBoolean = false
+
+//---------PART 2 -----------------
+
+let myString : String = "50"
+let myNumber1 : Int = 15
+let stringNumber : String = String(25)
+
+//define
+
+let myVariable : String
+
+//initialization
+
+myVariable = "Test"
+myVariable.uppercased()
+
+5*4
